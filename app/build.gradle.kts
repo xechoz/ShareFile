9a8220962220c0c2c9a4e6a41325bbe76b02e78c
xechoz.zheng <xechoz.zheng@gmail.com>
2026-09-17 19:47:14 +0800

    release: v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.xechoz.sharefile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
     }
 
     signingConfigs {
